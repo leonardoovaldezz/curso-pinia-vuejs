14745742ec921f24acbda704466ca30870319084
leonardoovaldezz <leonardocamvaldez12@hotmail.com>
2023-09-27 17:47:54 -0600

    pinia con las sintaxis tipo composition API

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,26 +1,45 @@
 import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
 
-export const useCounter = defineStore("counter", {
-  state: () => ({
-    count: 0,
-    name: 'Leonardo'
-  }),
-  getters: {
-    doubleCount: (state) => {
-      return state.count * 2
-    },
-    nameUppercase: (state) => {
-      return state.name.toUpperCase()
-    }
-  },
-  actions: {
-    increment() {
-      this.count++
-    },
-    reverseName() {
-      const reverse = this.name.split('').reverse().join('')
-      this.name = reverse
-    }
+export const useCounter = defineStore("counter", () => {
+  const count = ref(0)
+  const name = ref('Leoncin')
+  const doubleCount = computed(() => count.value * 2)
+  const nameUppercase = computed(() => name.value.toUpperCase())
+
+  function increment() {
+    count.value++
   }
-  //return { count, doubleCount, increment }
+
+  function reverseName() {
+    const reverse = name.value.split('').reverse().join('')
+    name.value = reverse
+  }
+
+  return { count, name, increment, reverseName, doubleCount, nameUppercase }
 })
+
+// export const useCounter = defineStore("counter", {
+//   state: () => ({
+//     count: 0,
+//     name: 'Leonardo'
+//   }),
+//   getters: {
+//     doubleCount: (state) => {
+//       return state.count * 2
+//     },
+//     nameUppercase: (state) => {
+//       return state.name.toUpperCase()
+//     }
+//   },
+//   actions: {
+//     increment() {
+//       this.count++
+//     },
+//     reverseName() {
+//       const reverse = this.name.split('').reverse().join('')
+//       this.name = reverse
+//     }
+//   }
+//   //return { count, doubleCount, increment }
+// })
